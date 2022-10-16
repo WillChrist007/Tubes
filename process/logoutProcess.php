588a9173c2ee236f97fc5b6d7f9459af3a2dd49a
Daniel <zikodaniel2@gmail.com>
2022-10-16 15:37:08 +0700

    ngerjain bagian punya Daniel

--- process/logoutProcess.php
+++ process/logoutProcess.php
@@ -1,7 +1,8 @@
 <?php 
-    ob_start(); 
-    session_start(); 
-    session_destroy();
-    
-    header("location: ../index.php"); 
+ 
+session_start();
+session_destroy();
+ 
+header("Location: index.php");
+ 
 ?>
