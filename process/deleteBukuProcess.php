a6c37a9807cece566bc0458f3298bbe5acbd3ffd
WillChrist007 <williamchristians007@gmail.com>
2022-10-17 21:34:26 +0700

    deleteBukuProcess

--- process/deleteBukuProcess.php
+++ process/deleteBukuProcess.php
@@ -3,9 +3,18 @@ session_start();
 if (isset($_GET['id'])) {
     include('../db.php');
     $id = $_GET['id'];
-    $queryDelete = mysqli_query($con, "DELETE FROM buku WHERE id='$id'") or
+
+    $querySelect = mysqli_query($con, "SELECT * FROM buku WHERE id='$id'") or die(mysqli_error($con));
+    
+    $book = mysqli_fetch_assoc($querySelect);
+    $_SESSION['book'] = $book;
+
+    $stock = $_SESSION['book']['stock'];
+    $sisa = $_SESSION['book']['sisa'];
+    
+    if ($stock == $sisa) {
+        $queryDelete = mysqli_query($con, "DELETE FROM buku WHERE id='$id'") or
         die(mysqli_error($con));
-    if ('stock'=='sisa') {
         echo
             '<script>
             alert("Delete Success"); window.location = "../page/admin/listBukuPage.php"
