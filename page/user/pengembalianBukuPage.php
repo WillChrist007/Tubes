913f8c4daf4773c380383387cd21d78db3f34045
Genome06 <genome0602@gmail.com>
2022-10-17 13:31:00 +0700

    pengembalianBukuPage and Process Done

--- page/user/pengembalianBukuPage.php
+++ page/user/pengembalianBukuPage.php
@@ -5,44 +5,69 @@ include '../../component/userSidebar.php'
 solid SaddleBrown; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0,
 0.19);">
     <div class="body d-flex justify-content-between">
-        <h4>LIST BUKU</h4>
-        <a href="../admin/addBukuPage.php"><i style="color: SaddleBrown" class="bi bi-plus-square-fill"> Tambah Buku</i></a>
+        <h4>LIST BUKU PEMINJAMAN</h4>
     </div>
     <hr>
     <table class="table ">
         <thead>
             <tr>
-                <th scope="col">No</th>
-                <th scope="col">Judul Buku</th>
+                <th scope="col">Nama Buku</th>
                 <th scope="col">Sampul</th>
                 <th scope="col">Status</th>
-                <th scope="col">Tanggal Pengembalian</th>
+                <th scope="col">Batas Tanggal Pengembalian</th>
                 <th scope="col">Aksi</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                $query = mysqli_query($con, "SELECT * FROM buku") or
+                $query = mysqli_query($con, "SELECT * FROM peminjaman") or
                 die(mysqli_error($con));
                 if (mysqli_num_rows($query) == 0) {
                     echo '<tr> <td colspan="7"> Tidak ada data </td> </tr>';
                 }else{
                     $no = 1;
-                    while($data = mysqli_fetch_assoc($query)){
-                    echo'
+                    while($data = mysqli_fetch_assoc($query)){  
+                    $id_buku = $data['id_buku'];
+                    $queryTwo = mysqli_query($con, "SELECT * FROM buku WHERE id = '$id_buku' ") or
+                     die(mysqli_error($con));     
+                    if($queryTwo){
+                        $dataTwo = mysqli_fetch_assoc($queryTwo);
+                        $tgl_pinjam = $data['tanggal_pinjam'];
+                        $tgl_kembali = date( "Y-m-d", strtotime( "$tgl_pinjam +7 day" ) );
+                        if($data['status'] == 1){
+                        echo'
                         <tr>
-                            <th scope="row">'.$no.'</th>
-                            <td>'.$data['judul'].'</td>
-                            <td><img src = ../../img/cover/' . $data['gambar'] .' style="height: 220px; width: 150px; border-radius: 5px"></td>
-                            <td>'.$data['status'].'</td>
-                            <td>'.$data['tgl_kembali'].'</td>
+                            <th scope="row">'.$dataTwo['judul'].'</td>
+                            <td><img src = ../../img/cover/' . $dataTwo['gambar'] .' style="height: 220px; width: 150px; border-radius: 5px"></td>
+                            <td>Dipinjam</td>
+                            <td>'.$tgl_kembali.'</td>
                             <td>
                                 <a href="../../process/pengembalianBukuProcess.php?id='.$data['id'].'
-                                "onClick="return confirm ( \'Are you sure want to return the book?\')"> 
-                                    <i style="color: red" class="bi bi-trash3-fill"></i>
-                            </td>
+                                    "onClick="return confirm ( \'Are you sure want to restore this book?\')">
+                                    <button type="submit" class="btn btn-primary" name="pinjam">Kembalikan</button>  
+                                </a>
+                            </td>    
                         </tr>';
-                    $no++;
+                        }else{
+                        echo'
+                        <tr>
+                            <th scope="row">'.$dataTwo['judul'].'</td>
+                            <td><img src = ../../img/cover/' . $dataTwo['gambar'] .' style="height: 220px; width: 150px; border-radius: 5px"></td>
+                            <td>Sudah Dikembalikan</td>
+                            <td>'.$tgl_kembali.'</td>
+                            <td>
+                                <a class="btn btn-primary disabled">
+                                    Kembalikan 
+                                </a>
+                            </td>    
+                        </tr>';
+                        }
+                    }else{
+                        echo'<script>
+                        alert("Gagal Mengakses Data");
+                        </script>';
+                    }
+                    $no++;    
                 }
                 }
             ?>
--- process/pengembalianBukuProcess.php
+++ process/pengembalianBukuProcess.php
@@ -1,20 +1,43 @@
 <?php
+
 session_start();
-if (isset($_POST['confirm'])) {
+if (isset($_GET['id'])) {
     include('../db.php');
 
-    $tgl_pinjam = date('Y-m-d');
+    $id_peminjaman = $_GET['id'];
+    $queryPinjam = mysqli_query($con, "SELECT * FROM peminjaman WHERE id = '$id_peminjaman' ") or
+    die(mysqli_error($con));
+    $data = mysqli_fetch_assoc($queryPinjam);
+    $id_buku = $data['id_buku']; 
+    $status = 0;
+    $queryPinjamTwo = mysqli_query($con, "SELECT * FROM buku WHERE id = '$id_buku' ") or
+    die(mysqli_error($con));
+    $dataTwo = mysqli_fetch_assoc($queryPinjamTwo);
+    $sisa = $dataTwo['sisa'] + 1;
+    $tgl_kembali = date('Y-m-d');
 
-    $query = mysql_query("UPDATE peminjaman SET tgl_kembali  = '$tgl', kembali = '2'
-        where id_pinjam ='$_GET[id]'");
-    if ($query) {
-        echo '<script>alert("Buku berhasil dikembalikan");
-        window.location = "../page/user/listBukuPage.php"</script>\n';
+
+    $query = mysqli_query($con, "INSERT INTO pengembalian (id_peminjaman, tanggal_kembali) VALUES ('$id_peminjaman', '$tgl_kembali')")
+    or die(mysqli_error($con));
+
+    $queryUpdate = mysqli_query($con, "UPDATE `peminjaman` SET `status`='$status'  WHERE id='$id_peminjaman'") 
+    or die(mysqli_error($con));
+    
+    $queryUpdateTwo = mysqli_query($con, "UPDATE `buku` SET `sisa`='$sisa' WHERE id='$id_buku'") 
+    or die(mysqli_error($con));
+
+    if ($query && $queryUpdate && $queryUpdateTwo) {
+        echo
+        '<script>
+        alert("Sukses Mengembalikan Buku");
+        window.location = "../page/user/listBukuPage.php"
+        </script>';
     } else {
-        echo '<script>alert("Gagal mengembalikan buku");
-        </script>\n';
+        echo
+        '<script>
+        alert("Gagal Mengembalikan Buku");
+        </script>';
     }
-
 } else {
     echo
     '<script>
